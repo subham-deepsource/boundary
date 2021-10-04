jweissig/boundary-1

refactor(schema): Add support for migration editions
This refactors the schema package and subpackages to support editions.
An Edition is a collection of sql migrations. A given boundary binary
can have multiple migration editions. The `schema.Manager` is updated to
apply the necessary migrations in the correct order, leveraging an
Edition's priority to determine the order in which the separate Editions
are applied.

The sql migration files are now nested under a directory for the
edition.

This also switches from the generated code to using embed, which
eliminates the need for the `make migrations` target and the generated
code. As such this commit removes the target and generated code.

See:
    https://pkg.go.dev/embed

internal/db/schema/migrations/base/postgres/02_log_migration.up.sql
@@ -1,0 +1,34 @@
+begin;
+create table log_migration(
+  id                bigint generated always as identity primary key,
+  migration_version bigint not null, -- cannot declare FK since the table is truncated during runtime
+  edition           text   not null, -- cannot declare FK since the table is truncated during runtime
+  create_time       timestamp with time zone default current_timestamp,
+  entry             text   not null
+);
+comment on table log_migration is
+'log_migration entries are logging output from databaes migrations';
+
+create or replace function
+  log_migration_version()
+  returns trigger
+as $$
+  declare current_version bigint;
+  begin
+    select max(version)
+	  from boundary_schema_version into current_version
+	 where edition = new.edition;
+    new.migration_version = current_version;
+    return new;
+  end;
+$$ language plpgsql;
+comment on function log_migration_version() is
+'log_migration_version will set the log_migration entries to the current migration version';
+
+create trigger
+  version_column
+before
+  insert on log_migration
+  for each row execute procedure log_migration_version();
+
+commit;

subham-deepsource/boundary

internal/db/schema/migrations/base/postgres/02_log_migration.up.sql
@@ -1,0 +1,34 @@
+begin;
+create table log_migration(
+  id                bigint generated always as identity primary key,
+  migration_version bigint not null, -- cannot declare FK since the table is truncated during runtime
+  edition           text   not null, -- cannot declare FK since the table is truncated during runtime
+  create_time       timestamp with time zone default current_timestamp,
+  entry             text   not null
+);
+comment on table log_migration is
+'log_migration entries are logging output from database migrations';
+
+create or replace function
+  log_migration_version()
+  returns trigger
+as $$
+  declare current_version bigint;
+  begin
+    select max(version)
+	  from boundary_schema_version into current_version
+	 where edition = new.edition;
+    new.migration_version = current_version;
+    return new;
+  end;
+$$ language plpgsql;
+comment on function log_migration_version() is
+'log_migration_version will set the log_migration entries to the current migration version';
+
+create trigger
+  version_column
+before
+  insert on log_migration
+  for each row execute procedure log_migration_version();
+
+commit;